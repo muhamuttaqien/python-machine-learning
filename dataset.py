8563df26a7c962adda629f2808151eac10836931
PT. Ruma Indonesia <angga.muttaqien@ruma.co.id>
2017-08-18 00:01:16 +0700

    Load and visualize the data

---
+++ dataset.py
@@ -0,0 +1,36 @@
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+print("# Load dataset")
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+print("# shape")
+print(dataset.shape)
+
+print("# head")
+print(dataset.head(20))
+
+print("# descriptions")
+print(dataset.describe())
+
+print("# class distribution")
+print(dataset.groupby('class').size())
+
+
+print("# box and whisker plots")
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
