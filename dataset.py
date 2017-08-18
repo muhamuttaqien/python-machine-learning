5d47fd83248c7be30592133bb930a309f740431f
Muh. Angga Muttaqien <angga.muttaqien@ruma.co.id>
2017-08-18 23:12:35 +0700

    Add other two types of visualization

--- dataset.py
+++ dataset.py
@@ -30,7 +30,14 @@ print(dataset.describe())
 print("# class distribution")
 print(dataset.groupby('class').size())
 
-
 print("# box and whisker plots")
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+print("# histograms")
+dataset.hist()
+plt.show()
+
+print("# scatter plot matrix")
+scatter_matrix(dataset)
 plt.show()
